a1df819a6fa139d4050058a7485ba15e30cfcf78
jessib <jessib@riseup.net>
2023-12-13 21:12:44 -0800

    initial stuff

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # kc-subject-lookup
 tool for lookup in King County WA Jail Lookup service
+
+Need node and npm
+ - works with node v20.10.0
+ - works with npm 10.2.3
+
+To get started do the following to install dependencies:
+
+```
+npm install
+```
+
+Then run the following to run the script:
+
+```
+node app.js
+```
+
+Should open a non-headless chromium browser and search for the names in the `lastNames` array
---
+++ app.js
@@ -0,0 +1,52 @@
+import puppeteer from 'puppeteer';
+
+// We will presumably want to read from a csv
+const lastNames = ['msadofhsaoi', "m*", "c*", "smith"];  //'miles'; //'brady'; //'cruz';
+
+// Open the installed Chromium. We use headless: false
+// to be able to inspect the browser window.
+const browser = await puppeteer.launch({
+    headless: false
+});
+
+// Open a new page / tab in the browser.
+const page = await browser.newPage();
+
+// Tell the tab to navigate to the JavaScript topic page.
+await page.goto('https://dajd-jms.powerappsportals.us/public/subject-lookup/');
+
+for (const lastName of lastNames) {
+
+    page.waitForSelector("input[name='0']");
+
+    await page.$eval("input[name='0']", el => el.value = '');
+    await page.type("input[name='0']", lastName);
+
+    //Wait a second before hitting submit, as was sometimes submitting before name in
+    await new Promise(r => setTimeout(r, 1000));
+
+    await page.click('.btn-entitylist-filter-submit');
+
+    await new Promise(r => setTimeout(r, 1000));
+
+    console.log("-------------------");
+    console.log("Searched for last name: " + lastName);
+    try {
+	let results = await page.$eval('table tbody', tbody => [...tbody.rows].map(r => [...r.cells].map(c => c.innerText)))
+
+	console.log("Result count: " + results.length);
+	results.map(row => {
+	    console.log("Name: " + row[1] + " Facility: " + row[3] + " Arresting Agency: " + row[4]);
+	});
+    } catch {
+	console.log("No results found.")
+    }
+    await new Promise(r => setTimeout(r, 1000));
+
+}
+
+// Pause for 10 seconds, to see what's going on.
+await new Promise(r => setTimeout(r, 10000));
+
+// Turn off the browser to clean up after ourselves.
+await browser.close();
